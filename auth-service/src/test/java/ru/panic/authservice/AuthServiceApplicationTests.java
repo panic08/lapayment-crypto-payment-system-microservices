5264f9492574670f3b33c4f6fc6700cc5207e1bc
Andrey <andrey1010102008@gmail.com>
2023-06-24 14:28:05 +0300

    Initial commit

---
+++ auth-service/src/test/java/ru/panic/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.panic.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
