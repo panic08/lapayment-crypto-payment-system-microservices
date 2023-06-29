f0e61d9556500cd7b2b4ac6f62482797c8bb5e14
Andrey <andrey1010102008@gmail.com>
2023-06-29 15:35:25 +0300

    added withdrawal service

---
+++ withdrawal-service/src/main/java/ru/panic/withdrawalservice/WithdrawalServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.panic.withdrawalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithdrawalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithdrawalServiceApplication.class, args);
+	}
+
+}
